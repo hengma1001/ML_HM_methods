9795822240326e0226789679ad07c29c60c779d0
Ma, Heng <maheng1991@hotmail.com>
2019-01-25 11:29:24 -0500

    change optimizer to adam

--- CVAE/.ipynb_checkpoints/vae_conv-checkpoint.py
+++ CVAE/.ipynb_checkpoints/vae_conv-checkpoint.py
@@ -2,6 +2,132 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F 
 
+
+class conv_variational_autoencoder(object): 
+    '''
+    variational autoencoder class 
+
+    parameters:
+      - image_size: tuple
+        height and width of images
+      - input_channels: int
+        number of channels in input images
+      - num_conv_layers: int 
+        number of encoding/decoding convolutional layers
+      - feature_map_list: list of ints
+        number of output feature maps for each convolutional layer
+      - filter_shape_list: list of tuples;
+        convolutional filter shape for each convolutional layer
+      - stride_list: list of tuples
+        convolutional stride for each convolutional layer
+      - num_dense_layers: int
+        number of encoding/decoding dense layers
+      - dense_neuron_list: list of ints
+        number of neurons for each dense layer
+      - dense_dropout_list: list of float
+        fraction of neurons to drop in each dense layer (between 0 and 1)
+      - latent_dim: int
+        number of dimensions for latent embedding
+      - activation: string (default='relu') 
+        activation function to use for layers
+      - eps_mean: float (default = 0.0) 
+        mean to use for epsilon (target distribution for embedding);
+      - eps_std: float (default = 1.0) 
+        standard dev to use for epsilon (target distribution for embedding)
+       
+    methods:
+      - train(data,batch_size,epochs=1,checkpoint=False,filepath=None)
+        train network on given data
+      - save(filepath)
+        save the model weights to a file
+      - load(filepath)
+        load model weights from a file
+      - return_embeddings(data)
+        return the embeddings for given data
+      - decode(embedding)
+        return a generated output given a latent embedding 
+      - predict(data) 
+        return the predicted result of data
+    '''
+    def __init__(self, image_size, input_channel, 
+                num_conv_layers, feature_map_list, filter_shape_list, stride_list, 
+                num_dense_layers, dense_neuron_list, dense_dropout_list, latent_dim,
+                activation_func='relu',
+                eps_mean=0.0,eps_std=1.0):   
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = CVAE_model(image_size, input_channel, 
+                          num_conv_layers, feature_map_list, filter_shape_list, stride_list, 
+                          num_dense_layers, dense_neuron_list, dense_dropout_list, latent_dim, 
+                          activation_func='relu', eps_mean=0.0,eps_std=1.0).to(self.device)
+#         self.optimizer = optim.RMSprop(self.model.parameters(), lr=0.001, alpha=0.9, eps=1e-08, weight_decay=1e-6)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+#         self.optimizer = optim.Adadelta(self.model.parameters(), lr=1e-1)
+        self.loss_func = nn.BCELoss(reduction='sum') 
+        
+    def encode(self, x): 
+        return self.model.encode(x) 
+    
+    def reparameterize(self, mu, logvar): 
+        return self.model.reparameterize(mu, logvar)
+    
+    def decode(self, z): 
+        return self.model.decode(z) 
+    
+    def return_embeddings(self, x): 
+        return self.model.reparameterize(*(self.model.encode(x))) 
+    
+    def predict(self, x): 
+        return self.model(x)[0] 
+    
+    def loss(self, recon_x, x, mu, logvar):  
+        BCE = self.loss_func(recon_x, x)
+
+        # see Appendix B from VAE paper:
+        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+        # https://arxiv.org/abs/1312.6114
+        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp()) 
+        return BCE + KLD 
+    
+    def train(self, train_loader, epoch):
+        self.model.train()
+        train_loss = 0
+        for batch_idx, (data, _) in enumerate(train_loader):
+            data = data.to(self.device)
+            self.optimizer.zero_grad()
+            recon_batch, mu, logvar = self.model(data)
+    #         print(recon_batch.shape, data.shape)
+            loss = self.loss(recon_batch, data, mu, logvar) 
+            loss.backward()
+            train_loss += loss.item()
+            self.optimizer.step()
+#             if batch_idx % log_interval == 0:
+#                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                     epoch, batch_idx * len(data), len(train_loader.dataset),
+#                     100. * batch_idx / len(train_loader),
+#                     loss.item() / len(data)))
+        print '====> Epoch: {} Average loss: {:.4f}'.format(epoch, train_loss / len(train_loader.dataset)),
+    
+        
+    def test(self, test_loader, epoch):
+        self.model.train()
+        test_loss = 0 
+        with torch.no_grad():
+            for i, (data, _) in enumerate(test_loader):
+                data = data.to(self.device) 
+                recon_batch, mu, logvar = self.model(data)
+                test_loss += self.loss(recon_batch, data, mu, logvar).item()
+#                 if i == 0:
+#                     n = min(data.size(0), 8)
+#                     comparison = torch.cat([data[:n],
+#                                           recon_batch.view(batch_size, 1, 28, 28)[:n]])
+#                     save_image(comparison.cpu(),
+#                              'results/reconstruction_' + str(epoch) + '.png', nrow=n)
+
+        test_loss /= len(test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))
+        
+
 class CVAE_model(nn.Module):
     '''
     variational autoencoder class;
@@ -41,16 +167,18 @@ class CVAE_model(nn.Module):
                 activation_func='relu',
                 eps_mean=0.0,eps_std=1.0): 
         super(CVAE_model, self).__init__() 
-        if len(feature_map_list) < num_conv_layers: 
-            raise Exception("The length of filter_map_list list must be more than number of convolutional layers. ") 
-        if len(filter_shape_list) < num_conv_layers: 
-            raise Exception("The length of filter_shape_list list must be more than number of convolutional layers. ") 
-        if len(stride_list) < num_conv_layers: 
-            raise Exception("The length of stride_list list must be more than number of convolutional layers. ") 
-        if len(dense_neuron_list) < num_dense_layers: 
-            raise Exception("The length of dense_neuron_list list must be more than number of dense layers. ") 
-        if len(dense_dropout_list) < num_dense_layers: 
-            raise Exception("The length of dense_dropout_list list must be more than number of dense layers. ") 
+        self.eps_mean = eps_mean
+        self.eps_std = eps_std
+        if len(feature_map_list) != num_conv_layers: 
+            raise Exception("The length of filter_map_list list must equal the number of convolutional layers. ") 
+        if len(filter_shape_list) != num_conv_layers: 
+            raise Exception("The length of filter_shape_list list must equal the number of convolutional layers. ") 
+        if len(stride_list) != num_conv_layers: 
+            raise Exception("The length of stride_list list must equal the number of convolutional layers. ") 
+        if len(dense_neuron_list) != num_dense_layers: 
+            raise Exception("The length of dense_neuron_list list must equal the number of dense layers. ") 
+        if len(dense_dropout_list) != num_dense_layers: 
+            raise Exception("The length of dense_dropout_list list must equal the number of dense layers. ") 
         
         self.image_size = image_size 
         self.input_channel = input_channel 
@@ -78,7 +206,8 @@ class CVAE_model(nn.Module):
         self.encode_conv_layers = nn.ModuleList(self.encode_conv_layers)
             
         ## Encoding dense layers 
-        mat_size *= feature_map_list[-1] 
+        if feature_map_list: 
+            mat_size *= feature_map_list[-1] 
         self.conv_dense_dim = mat_size
         self.encode_dense_layers = [] 
         self.encode_dropout_layers = []
@@ -138,7 +267,7 @@ class CVAE_model(nn.Module):
         return mu, logvar 
     
     def reparameterize(self, mu, logvar): 
-        # eps * exp(-0.5 * logvar) * mu 
+        # eps * exp(-0.5 * logvar) + mu 
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
         return eps.mul(std).add_(mu)
